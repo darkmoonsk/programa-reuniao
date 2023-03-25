d70f9565e49a0fbe41c3ac39a5655ca924a18e90
Bruno Souza <brunoezully@gmail.com>
2023-03-25 19:00:25 -0300

    Separando a logica em um arquivo de script

---
+++ script.js
@@ -0,0 +1,7 @@
+const meuTexto = document.querySelectorAll("input");
+    const inputs = Array.from(meuTexto);
+
+    inputs.map(input => input.setAttribute("size", input.value.trim().length  + "px"));
+    inputs.map(input => input.addEventListener('input', function () {
+    this.setAttribute("size", this.value.replace(/\s+/g, "").length + (this.value.match(/ /g) || []).length / 1.6);
+}));
