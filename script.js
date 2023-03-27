6d54eaf7e4cc9a435a2c54224d62d252ccc89cc3
Bruno Souza <brunoezully@gmail.com>
2023-03-26 22:09:51 -0300

    ADD: Botão para retirar e colocar um tema

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const btnDelete1 = document.querySelector("#btn-delete1");
+const temaSemana1 = document.querySelector("#tema-semana1");
+const nameToQuit1 = document.querySelector("#name-to-quit1");
+const temaSemana2 = document.querySelector("#tema-semana2");
+const nameToQuit2 = document.querySelector("#name-to-quit2");
+const btnDelete2 = document.querySelector("#btn-delete2");
 
 
 const meuTexto = document.querySelectorAll("input");
@@ -7,4 +13,14 @@ const meuTexto = document.querySelectorAll("input");
     inputs.map(input => input.addEventListener('input', function () {
     this.setAttribute("size", this.value.replace(/\s+/g, "").length + (this.value.match(/ /g) || []).length / 1.6);
 }));
+
+btnDelete1.addEventListener("click", () => {
+    temaSemana1.classList.toggle("hidden");
+    nameToQuit1.classList.toggle("hidden");
+});
+
+btnDelete2.addEventListener("click", () => {
+    temaSemana2.classList.toggle("hidden");
+    nameToQuit2.classList.toggle("hidden");
+});
 
